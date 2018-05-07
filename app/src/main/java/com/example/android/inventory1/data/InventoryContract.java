b5c2a59591606414b44cf480089ab3aeebf3c5b1
saidabxl <qunsulka@gmail.com>
2018-05-07 22:30:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/inventory1/data/InventoryContract.java
@@ -0,0 +1,32 @@
+package com.example.android.inventory1.data;
+
+import android.provider.BaseColumns;
+
+public class InventoryContract {
+
+
+    public InventoryContract() {
+    }
+
+    public final static class InventoryEntry implements BaseColumns {
+
+
+        public final static String TABLE_NAME = "product";
+
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PRODUCT_NAME = "product_name";
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public final static String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
+        public final static String COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER = "supplier_phone_number";
+
+
+        public final static int SUPPLIER_UNKNOWN = 0;
+        public final static int SUPPLIER_AMAZON = 1;
+        public final static int SUPPLIER_EBAY = 2;
+        public final static int SUPPLIER_LULU = 3;
+
+
+    }
+}
